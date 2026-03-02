b811f23c895d6f65a520e899052a7d170ac0f1e3
sonal-jakhar <sonaljakhar2004@gmail.com>
2026-03-02 14:57:09 +0530

    Add home page UI with hero and book components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import HeroSection from "@/components/HeroSection";
+import { sampleBooks } from "@/lib/constants";
+import BookCard from "@/components/BookCard";
 
-const page = () => {
-  return <div></div>;
+const Page = () => {
+  return (
+    <main className="wrapper container">
+      <HeroSection />
+
+      <div className="library-books-grid">
+        {sampleBooks.map((book) => (
+          <BookCard
+            key={book._id}
+            title={book.title}
+            author={book.author}
+            coverURL={book.coverURL}
+            slug={book.slug}
+          />
+        ))}
+      </div>
+    </main>
+  );
 };
 
-export default page;
+export default Page;
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [{ protocol: "https", hostname: "covers.openlibrary.org" }],
+    dangerouslyAllowLocalIP: true,
+  },
 };
 
 export default nextConfig;
